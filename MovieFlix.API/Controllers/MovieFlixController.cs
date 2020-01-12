5f0c0d90ab71b017551d04f80a912ea77ffcad14
jaime_constanzo <jaime.constanzo@external.wolterskluwer.com>
2020-01-12 12:31:07 +0100

    Sacados los metodos de recomendaciones a un servicio

--- MovieFlix.API/Controllers/MovieFlixController.cs
+++ MovieFlix.API/Controllers/MovieFlixController.cs
@@ -4,17 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 using ServiceStack.Redis;
 using System.Collections.Generic;
 using MovieFlix.Application;
+using MovieFlix.Application.Services;
 
 namespace MovieFlix.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class MovieFlixController : ControllerBase
-    { 
+    {
+        private RecommendationService recommendationService;
+
         private const string REDIS_SERVER_CONNECTION = "localhost:6379";
         private const string TOP_PELIS_SORTEDSET = "TOP10";
 
         private readonly RedisManagerPool Manager = new RedisManagerPool(REDIS_SERVER_CONNECTION);
+
+        public MovieFlixController()
+        {
+            recommendationService = new RecommendationService();
+        }
 
         [HttpGet]
         public string Get()
@@ -55,59 +63,7 @@ namespace MovieFlix.Controllers
         [HttpPost("WatchMovie")]
         public void WatchMovie([FromBody] MovieVisualization movieVisualization)
         {
-            UpdateRecommendations(movieVisualization);
+            recommendationService.UpdateRecommendations(movieVisualization);
         }
-
-        #region Private Methods
-        private void UpdateRecommendations(MovieVisualization movieVisualization)
-        {
-            using (var client = Manager.GetClient())
-            {
-                //Incremento en TOP Pelis
-                client.IncrementItemInSortedSet(TOP_PELIS_SORTEDSET, movieVisualization.MovieName, 1);
-
-                //Incremento en TOP Pelis por su género
-                client.IncrementItemInSortedSet(movieVisualization.GenreName, movieVisualization.MovieName, 1);
-            }
-
-            UpdateCustomUserRecommendations(movieVisualization.UserId);
-        }
-
-        private void SaveViewsDb(MovieVisualization view)
-        {
-            //insertamos la visita en SQL
-        }
-
-        private Dictionary<string, int> UpdateCustomUserRecommendations(string userId) 
-        {
-            Dictionary<string, int> genreViews = new Dictionary<string, int>() { { "Action", 247 }, { "Love", 60 }, { "Comedy", 10 }, }; // get genre views from bd
-
-            int viewsCount = genreViews.Sum(x => x.Value);
-
-            int totalRecomendations = 20;
-
-            Dictionary<string, int> genreScores = new Dictionary<string, int>();
-
-            foreach (var genre in genreViews.Keys) 
-            {
-                var genreRecomedation = ((genreViews[genre] * (double) 100 / viewsCount) * totalRecomendations) / 100;
-                genreScores.Add(genre, (int) Math.Round(genreRecomedation, 0));
-            }
-
-            var recoCount = genreScores.Sum(x => x.Value);
-
-            if (recoCount > totalRecomendations) 
-            {
-                var lastKey = genreScores.Keys.Last();
-
-                if (genreScores[lastKey] - 1 == 0)
-                    genreScores.Remove(lastKey);
-                else
-                    genreScores[lastKey] -= 1;                
-            }
-
-            return genreScores;
-        }
-        #endregion
     }
 }
---
+++ MovieFlix.Application/Services/RecommendationService.cs
@@ -0,0 +1,65 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieFlix.Application.Services
+{
+    public class RecommendationService
+    {
+        private const string REDIS_SERVER_CONNECTION = "localhost:6379";
+        private const string TOP_PELIS_SORTEDSET = "TOP10";
+
+        private readonly RedisManagerPool Manager = new RedisManagerPool(REDIS_SERVER_CONNECTION);
+
+        public void UpdateRecommendations(MovieVisualization movieVisualization)
+        {
+            using (var client = Manager.GetClient())
+            {
+                //Incremento en TOP Pelis
+                client.IncrementItemInSortedSet(TOP_PELIS_SORTEDSET, movieVisualization.MovieName, 1);
+
+                //Incremento en TOP Pelis por su género
+                client.IncrementItemInSortedSet(movieVisualization.GenreName, movieVisualization.MovieName, 1);
+            }
+
+            UpdateCustomUserRecommendations(movieVisualization.UserId);
+        }
+
+        public void SaveViewsDb(MovieVisualization view)
+        {
+            //insertamos la visita en SQL
+        }
+
+        public Dictionary<string, int> UpdateCustomUserRecommendations(string userId)
+        {
+            Dictionary<string, int> genreViews = new Dictionary<string, int>() { { "Action", 247 }, { "Love", 60 }, { "Comedy", 10 }, }; // get genre views from bd
+
+            int viewsCount = genreViews.Sum(x => x.Value);
+
+            int totalRecomendations = 20;
+
+            Dictionary<string, int> genreScores = new Dictionary<string, int>();
+
+            foreach (var genre in genreViews.Keys)
+            {
+                var genreRecomedation = ((genreViews[genre] * (double)100 / viewsCount) * totalRecomendations) / 100;
+                genreScores.Add(genre, (int)Math.Round(genreRecomedation, 0));
+            }
+
+            var recoCount = genreScores.Sum(x => x.Value);
+
+            if (recoCount > totalRecomendations)
+            {
+                var lastKey = genreScores.Keys.Last();
+
+                if (genreScores[lastKey] - 1 == 0)
+                    genreScores.Remove(lastKey);
+                else
+                    genreScores[lastKey] -= 1;
+            }
+
+            return genreScores;
+        }
+    }
+}
