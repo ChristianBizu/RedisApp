201bfae064a0941da88e3c88f1742a45924219d8
Gonzalo López <gonzalo.lopez@cognodata.com>
2020-01-15 21:56:51 +0100

    Quito el not null del campo fecha de visualizaciones porque no lo usamos y me da problemas al insertar masivamente

--- MovieFlixSQL/dbo/Tables/Visualizations.sql
+++ MovieFlixSQL/dbo/Tables/Visualizations.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Visualizations] (
     [UserId]            NVARCHAR (32) NOT NULL,
     [MovieId]           INT           NOT NULL,
-    [DateVisualization] DATETIME      NOT NULL,
+    [DateVisualization] DATETIME,
     CONSTRAINT [PK_Visualizations] PRIMARY KEY CLUSTERED ([UserId] ASC, [MovieId] ASC, [DateVisualization] ASC)
 );
 
